f1b675d633ccd2b6777127e38b2d7a2af43aee15
shinya-watanabe <shinya-watanabe@cookpad.com>
2022-10-17 15:01:54 +0900

    Fix test

--- search_v2_test.go
+++ search_v2_test.go
@@ -9,23 +9,11 @@ func TestSearchPositionV2(t *testing.T) {
 	solved := false
 	cond := SearchCondition{
 		BitField:       NewBitFieldWithMattulwan("a62gacbagecb2ae2g3"),
-		PuyoSets:       []PuyoSet{{Red, Blue}},
-		DisableChigiri: true,
+		PuyoSets:       []PuyoSet{{Red, Blue}, {Green, Blue}},
+		DisableChigiri: false,
 		LastCallback: func(sr *SearchResult) {
-			if sr.RensaResult.Chains == 0 {
-				newCond := SearchCondition{
-					BitField:       sr.BeforeSimulate,
-					PuyoSets:       []PuyoSet{{Green, Blue}},
-					DisableChigiri: true,
-					LastCallback: func(sr2 *SearchResult) {
-						if sr2.RensaResult.Chains == 3 {
-							sr2.BeforeSimulate.ShowDebug()
-							solved = true
-							fmt.Println(sr2.BeforeSimulate.MattulwanEditorUrl())
-						}
-					},
-				}
-				newCond.SearchWithPuyoSetsV2()
+			if sr.RensaResult.Chains == 3 {
+				solved = true
 			}
 		},
 	}
